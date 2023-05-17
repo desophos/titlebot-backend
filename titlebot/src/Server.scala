09e93890b7860e50f6984549a79ef4ef4600d6ae
desophos <1887450+desophos@users.noreply.github.com>
2023-05-16 21:51:16 -0500

    Add CORS and compose middleware

--- titlebot/src/Server.scala
+++ titlebot/src/Server.scala
@@ -6,6 +6,7 @@ import org.http4s.*
 import org.http4s.dsl.io.*
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.middleware.CORS
 import org.http4s.server.middleware.ErrorAction
 import org.http4s.server.middleware.ErrorHandling
 import org.http4s.server.middleware.Logger
@@ -13,31 +14,35 @@ import scribe.cats.{io => log}
 
 object Server {
   def run = {
-    for {
-      client <- EmberClientBuilder.default[IO].build
-      getTitle = GetTitle.impl(client)
-      routes   = Routes.getTitleRoutes(getTitle).orNotFound
+    val addCors = CORS.policy.withAllowOriginAll.apply[IO, IO]
 
-      logErrorsApp = ErrorHandling.Recover.total(
+    def addErrorLogging(app: Http[IO, IO]) =
+      ErrorHandling.Recover.total(
         ErrorAction.log(
-          routes,
+          app,
           log.debug(_, _),
           log.warn(_, _),
         )
       )
 
-      finalHttpApp = Logger.httpApp(
-        logHeaders = true,
-        logBody = true,
-        redactHeadersWhen = _ => false,
-        logAction = Some(IO.println),
-      )(logErrorsApp)
+    val addHttpLogging = Logger.httpApp(
+      logHeaders = true,
+      logBody = true,
+      redactHeadersWhen = _ => false,
+      logAction = Some(IO.println),
+    )
+
+    for {
+      client <- EmberClientBuilder.default[IO].build
+      getTitle = GetTitle.impl(client)
+      routes   = Routes.getTitleRoutes(getTitle).orNotFound
+      app = (addCors compose addErrorLogging compose addHttpLogging)(routes)
 
       _ <- EmberServerBuilder
         .default[IO]
         .withHost(ipv4"0.0.0.0")
         .withPort(port"8080")
-        .withHttpApp(finalHttpApp)
+        .withHttpApp(app)
         .build
     } yield ()
   }.useForever
