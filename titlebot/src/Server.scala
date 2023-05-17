da5c724de356c30645c81d9c2d080dccd3b448dc
desophos <1887450+desophos@users.noreply.github.com>
2023-05-17 15:47:29 -0500

    Add client middleware for logging & redirects

--- titlebot/src/Server.scala
+++ titlebot/src/Server.scala
@@ -3,6 +3,8 @@ package titlebot
 import cats.effect.*
 import com.comcast.ip4s.*
 import org.http4s.*
+import org.http4s.client.middleware.{Logger => ClientLogger}
+import org.http4s.client.middleware.FollowRedirect
 import org.http4s.dsl.io.*
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
@@ -10,7 +12,6 @@ import org.http4s.server.middleware.{Logger => ServerLogger}
 import org.http4s.server.middleware.CORS
 import org.http4s.server.middleware.ErrorAction
 import org.http4s.server.middleware.ErrorHandling
-import org.http4s.server.middleware.Logger
 import scribe.cats.{io => log}
 
 object Server {
@@ -36,8 +37,20 @@ object Server {
     val serverMiddleware =
       (serverCors compose serverErrorLogging compose serverHttpLogging)
 
+    val clientFollowRedirect = FollowRedirect[IO](5, _ => false)
+
+    val clientHttpLogging = ClientLogger(
+      logHeaders = true,
+      logBody = true,
+      redactHeadersWhen = _ => false,
+      logAction = Some(log.debug(_)),
+    )
+
+    val clientMiddleware = (clientFollowRedirect compose clientHttpLogging)
+
     for {
-      client <- EmberClientBuilder.default[IO].build
+      baseClient <- EmberClientBuilder.default[IO].build
+      client   = clientMiddleware(baseClient)
       getTitle = GetTitle.impl(client)
       routes   = Routes.getTitleRoutes(getTitle).orNotFound
       app      = serverMiddleware(routes)
