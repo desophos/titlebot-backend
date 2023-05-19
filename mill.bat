bf7894fbce343543f6808f48ddbe3fb34382edf1
desophos <1887450+desophos@users.noreply.github.com>
2023-05-19 13:21:40 -0500

    Fix SSL revocation error in mill.bat

--- mill.bat
+++ mill.bat
@@ -95,7 +95,7 @@ if not exist "%MILL%" (
     rem but I don't think we can expect all the users to have it in 2019
     where /Q curl
     if %ERRORLEVEL% EQU 0 (
-        curl -f -L "!DOWNLOAD_URL!" -o "!DOWNLOAD_FILE!"
+        curl -f -L "!DOWNLOAD_URL!" -o "!DOWNLOAD_FILE!" --ssl-no-revoke
     ) else (
         rem bitsadmin seems to be available on Windows 7
         rem without /dynamic, github returns 403
